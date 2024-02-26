f35567cd4520257ddaebf7266e82cd7ddcb6802c
Carlota de la Vega <carlotadlavega@correo.ugr.es>
2024-02-26 15:05:03 +0100

    Añadir a lista
    Se añade a la lista la persona, pero falta el inicio de sesión para poder decirle a que usuario está ligado. No sé si esta bien la estructura

--- lib/controllers/users_controller.dart
+++ lib/controllers/users_controller.dart
@@ -6,4 +6,9 @@ class UserController{
   Future<List<Map<String, dynamic>>> users() async{
     return await db.users();
   }
+
+  Future<List<Map<String, dynamic>>> addToList(String name, int age, String nationality, String relationship, DateTime kissDate, String observations) async{
+    return await db.addToList(name, age, nationality, relationship, kissDate, observations);
+  }
+
 }
--- lib/db/db.dart
+++ lib/db/db.dart
@@ -1,14 +1,15 @@
 import 'package:postgres/postgres.dart';
 
-class DB{
-  
-  PostgreSQLConnection? conexion = PostgreSQLConnection('dumbo.db.elephantsql.com', 5432, 'hjtkdjnd', username: 'hjtkdjnd', password: 'W5JAZLYguuhy9pyFrHDiFa5QYcsnu78c');
+class DB {
+  PostgreSQLConnection? conexion = PostgreSQLConnection(
+      'dumbo.db.elephantsql.com', 5432, 'hjtkdjnd',
+      username: 'hjtkdjnd', password: 'W5JAZLYguuhy9pyFrHDiFa5QYcsnu78c');
 
   conectar() async {
     if (conexion?.isClosed == true) await conexion?.open();
   }
 
-  desconectar(){
+  desconectar() {
     conexion?.close();
   }
 
@@ -17,25 +18,37 @@ class DB{
   ///     FUNCIONES PARA LAS TAREAS                             ///
   ///                                                           ///
   /////////////////////////////////////////////////////////////////
-  
+
   // Función para ejecuciones de sentencias bd:
-  Future<List<Map<String, Map<String, dynamic>>>> ejecutar(String sentencia) async{
+  Future<List<Map<String, Map<String, dynamic>>>> ejecutar(
+      String sentencia) async {
     List<Map<String, Map<String, dynamic>>> resultado = [];
 
     await conectar();
 
-    try{
+    try {
       resultado = await conexion!.mappedResultsQuery(sentencia);
       return resultado;
-    } catch(e){
+    } catch (e) {
       print('ERROR: $e');
       return [];
-    } 
+    }
   }
 
-  // Devolver todas las tareas:
-  Future<List<Map<String, dynamic>>> users() async{
+  // Devolver todos los usuarios:
+  Future<List<Map<String, dynamic>>> users() async {
     return await ejecutar("select * from users");
   }
 
+  // Añadir una persona a la lista:
+  Future<List<Map<String, dynamic>>> addToList(
+      String name,
+      int age,
+      String nationality,
+      String relationship,
+      DateTime kissDate,
+      String observations) async {
+    return await ejecutar(
+        "insert into list (name, age, nationality, relationship, kiss_date, observations) values ('$name', $age, '$nationality', '$relationship', '$kissDate', '$observations')");
+  }
 }
