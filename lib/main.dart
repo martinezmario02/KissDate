86c2caf2d1f8b6c01fe29270054a670865915f83
Carlota de la Vega <carlotadlavega@correo.ugr.es>
2024-03-10 13:23:45 +0100

    Menu inicio. No tiene funcionalidad, solo añade una nueva opcion

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'screens/components/add.dart';
 import 'screens/components/list.dart';
 import 'screens/components/statistics.dart';
 import 'screens/users/login.dart';
+import 'screens/users/register.dart';
+
 
 void main() {
   runApp(const KissDate());
@@ -21,10 +23,14 @@ class KissDate extends StatelessWidget {
               seedColor: const Color.fromARGB(255, 243, 105, 137)),
           useMaterial3: true,
         ),
-        home: const Login(titulo: 'Kiss Date'),
+        home: const StartMenu(),
         routes: {
           '/lib/screens/menu.dart': (context) =>
               const MainMenu(titulo: 'Kiss Date'),
+          '/lib/screens/users/login.dart': (context) =>
+              const Login(titulo: 'Iniciar sesión'),
+          '/lib/screens/users/register.dart': (context) =>
+              const Register(titulo: 'Registrarse'),
           '/lib/screens/people/add.dart': (context) =>
               const AddPerson(titulo: 'Añadir a la lista'),
           '/lib/screens/people/list.dart': (context) =>
@@ -32,5 +38,87 @@ class KissDate extends StatelessWidget {
           '/lib/screens/resume.dart': (context) =>
               const Summary(titulo: 'Resumen / Estadísticas'),
         });
+  }
+}
+
+// Tres botones: Iniciar sesión, Registrarse, Invitado
+class StartMenu extends StatefulWidget {
+  const StartMenu({super.key});
+
+  @override
+  State<StartMenu> createState() => _StartMenuState();
+}
+
+class _StartMenuState extends State<StartMenu> {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Kiss Date'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 100,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/lib/screens/users/login.dart');
+                  },
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 15),
+                      const SizedBox(width: 15),
+                      Text('Iniciar sesión',
+                          style: TextStyle(fontSize: screenWidth * 0.05)),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                height: 100,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/lib/screens/users/register.dart');
+                  },
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 15),
+                      const SizedBox(width: 15),
+                      Text('Registrarse',
+                          style: TextStyle(fontSize: screenWidth * 0.05)),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                height: 100,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/lib/screens/menu.dart');
+                  },
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 15),
+                      const SizedBox(width: 15),
+                      Text('Invitado',
+                          style: TextStyle(fontSize: screenWidth * 0.05)),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
