7cc9e43a756976754eb9512861039d08e924fd61
Carlota de la Vega <carlotadlavega@correo.ugr.es>
2024-03-11 12:50:20 +0100

    Contraseñas codificadas. Closes #31

--- lib/db/users_db.dart
+++ lib/db/users_db.dart
@@ -1,8 +1,8 @@
+import 'package:bcrypt/bcrypt.dart';
 import 'db.dart';
 
 /// Class that represents the database for users.
-class UserDB extends DB{
-
+class UserDB extends DB {
   /// Get the list of users.
   Future<List<Map<String, dynamic>>> users() async {
     return await ejecutar("select * from users");
@@ -14,10 +14,15 @@ class UserDB extends DB{
   /// [email] - Email of the user.
   /// [birthday] - Date of birth of the user.
   /// [password] - Password of the user.
+  ///
+  /// Returns the list of users.
   Future<List<Map<String, dynamic>>> addUser(
       String name, String email, DateTime birthday, String password) async {
+    String salt = BCrypt.gensalt();
+    String hashedPassword = BCrypt.hashpw(password, salt);
+
     return await ejecutar(
-        "insert into users (name, mail, birthday, password) values ('$name', '$email', '$birthday', '$password')");
+        "INSERT INTO users (name, mail, birthday, password, salt) VALUES ('$name', '$email', '$birthday', '$hashedPassword', '$salt')");
   }
 
   /// Login a user.
@@ -28,12 +33,25 @@ class UserDB extends DB{
   /// Returns the identifier of the user if the login is successful, -1 otherwise.
   Future<int> login(String email, String password) async {
     var result = [];
-    result = await ejecutar(
-        "select user_id from users where mail='$email' and password='$password'");
+    result = await ejecutar("select * from users where mail='$email'");
+
     if (result.isEmpty) {
       return -1;
     } else {
-      return result[0]['users']['user_id'];
+      var salt = result[0]['users']['salt'];
+      var storedPassword = result[0]['users']['password'];
+
+      if (salt != null && storedPassword != null) {
+        var hashedPassword = BCrypt.hashpw(password, salt);
+
+        if (hashedPassword == storedPassword) {
+          return result[0]['users']['user_id'];
+        } else {
+          return -1;
+        }
+      } else {
+        return -1;
+      }
     }
   }
 }
